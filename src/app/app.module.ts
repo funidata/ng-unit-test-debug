97d0234649146621855c32270cfc24654765c684
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-22 14:07:20 +0300

    Add sample test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChildComponent, Test1Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'child',
+  template: '<span>Just a child component.</span>',
+})
+export class ChildComponent {}
