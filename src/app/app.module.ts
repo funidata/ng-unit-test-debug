beb836846e1704609aed2928b550f5676abd41b1
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-09-22 14:29:23 +0300

    Install transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { Test1Component } from './test1/test1.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent, ChildComponent, Test1Component],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
